61bbb183bb0f946ea5ff1f2b0228810d9c52b058
mike <72279453+belikemikeunderscore@users.noreply.github.com>
2023-10-27 22:04:17 +0000

    Update game.cpp
    code beautifier (looked bad b4)

--- game.cpp
+++ game.cpp
@@ -1,40 +1,43 @@
 #include <iostream>
+
 #include <ctime>
+
 #include <cstdlib>
+
 using namespace std;
 
+int main() {
 
-int main(){
-
-    srand((unsigned) time(0));
-    int numSorte = rand() % 100;
-    int num = 0;
-    int tries = 0;
-    int maxtries;
-    string nome;
-    cout << "whats your name ma friend\n";
-    cin >> nome;
-    cout << "how many tries you want ma friend\n";
-    cin >> maxtries;
-    while(num != numSorte){
-        while (tries != maxtries){
-    cout << "whats the lucky numba" << endl;
-    cin >> num;
-    if(numSorte > num){
+  srand((unsigned) time(0));
+  int numSorte = rand() % 100;
+  int num = 0;
+  int tries = 0;
+  int maxtries;
+  string nome;
+  cout << "whats your name ma friend\n";
+  cin >> nome;
+  cout << "how many tries you want ma friend\n";
+  cin >> maxtries;
+  while (num != numSorte) {
+    while (tries != maxtries) {
+      cout << "whats the lucky numba" << endl;
+      cin >> num;
+      if (numSorte > num) {
         tries++;
-        cout << "lucky numba is HIGHER than " << num << endl;}
-    else if (numSorte < num) {
+        cout << "lucky numba is HIGHER than " << num << endl;
+      } else if (numSorte < num) {
         tries++;
-        cout << "lucky numba is LOWER than " << num << endl;}
-    else {
+        cout << "lucky numba is LOWER than " << num << endl;
+      } else {
         cout << "U GOT IT in " << tries << " TRIES " << nome << "!!!!!!!!!!!\n";
-        break;} 
+        break;
+      }
 
-        }
-        if (num != numSorte && tries == maxtries) {
-            cout << "\noops your ran outta tries it was actually " << numSorte << "\n";
-            break;
-        }
-    return 0;
     }
+    if (num != numSorte && tries == maxtries) {
+      cout << "\noops your ran outta tries it was actually " << numSorte << "\n";
+      break;
+    }
+    return 0;
+  }
 }
